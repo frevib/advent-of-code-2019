2e92c384f2c946ed52a8ef4c439a65d5dbd4afdc
Hielke de Vries <hdevries@fastmail.com>
2019-12-02 21:19:11 +0100

    add day 2 star 1

---
+++ app/Day02.hs
@@ -0,0 +1,45 @@
+module Day02 where
+
+import Data.Char
+import Data.List.Split
+import Data.Function
+import Debug.Trace
+
+main :: IO ()
+main = do
+    content <- readFile "resources/day02.txt"
+    -- let contentList = ["1","0","0","3"]
+    -- let contentList = ["1","1","1","4","99","5","6","0","99"]
+    let contentList = splitOn "," content
+    print $ "star 1: " ++ show (head (processCode 0 $ map read contentList))
+
+
+processCode :: Int -> [Int] -> [Int]
+processCode offset input
+    | (head (drop offset input)) == 99 = input
+    | otherwise = 
+        let offsetInput = drop offset input
+            (a : b : c : d : xs) = offsetInput
+        in
+            if a == 1
+                then 
+                    let calculated = (input !! b) + (input !! c)
+                        newInput = replaceNth d calculated input
+                    in 
+                        processCode (offset + 4) newInput
+            else if a == 2
+                then 
+                    let calculated = (input !! b) * (input !! c)
+                        newInput = replaceNth d calculated input
+                    in 
+                        processCode (offset + 4) newInput
+            else 
+                []
+
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth n newVal (x:xs)
+    | n == 0 = newVal:xs
+    | otherwise = x:replaceNth (n-1) newVal xs
+
+debug = flip trace
