7e3db485a0e668571f44a99e84570447a99f80e6
Hielke de Vries <hdevries@fastmail.com>
2019-12-09 20:40:56 +0100

    intcode compiler refactored and working

--- app/Day09.hs
+++ app/Day09.hs
@@ -11,113 +11,100 @@ type Program = [Int] -> Int
 
 main :: IO ()
 main = do
-    content <- readFile "resources/day07.txt"
+    content <- readFile "resources/day05.txt"
     let program = map read (splitOn "," content)
 
-    let test1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99] ++ (replicate 30000 0)
+    -- let test1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99] ++ (replicate 30000 0)
         
+    let testEqual = [3,9,8,9,10,9,4,9,99,-1,8]
+    let testLessThan = [3,3,1107,-1,8,3,4,3,99]
+    let jumpTest = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
 
+    let maxValueStar1 = head (processCode 1 [] 0 program)
+    let maxValueStar2 = head (processCode 5 [] 0 program)
 
-    let phasesTestStar2 = [8,7,6,5]
-    let phases = phasesTestStar2
-    let programStates = [program, program, program, program, program]
-    let instructionPointers = [0, 0, 0, 0, 0]
+    let test1 = maxValueStar1 == 9961446
+    let test2 = maxValueStar2 == 742621
+    
 
-    let maxValueStar1 = maximum (map (\x -> 
-                    let (a : b : c : d : e) = x
-                    in processCode [b, c, d, head e] 0 [a,0] 0 0 program instructionPointers programStates
-                ) (permutations [0..4]))
-    let maxValueStar2 = maximum (map (\x ->
-                    let (a : b : c : d : e) = x
-                    in processCode [b, c, d, head e] 0 [a,0] 0 0 program instructionPointers programStates
-                ) (permutations [5..9]) )
-    let test1 = maxValueStar1 == 929800
-    let test2 = maxValueStar2 == 15432220
-
-    print $ "star 1: " ++ show maxValueStar1
-    print $ "star 2: " ++ show maxValueStar2
+    -- print $ "star 1: " ++ show (head (processCode 1 [] 0 program))
+    -- print $ "star 2: " ++ show (head (processCode 5 [] 0 program))
     print $ "tests: " ++ show test1 ++ ", " ++ show test2
 
-processCode :: [Int] -> Int -> [Int] -> Int -> Int -> [Int] -> [Int] -> [[Int]] -> Int
-processCode phases amp input output offset program instructionPointers programStates =
-    case (head (drop offset program)) `mod` 100 of
-        1 -> sum1 program offset phases amp input output instructionPointers programStates
-        2 -> multiply program offset phases amp input output instructionPointers programStates
-        3 -> input' program offset phases amp input output instructionPointers programStates
-        4-> output' program offset phases amp input output instructionPointers programStates
-        5 -> jumpIfTrue program offset phases amp input output instructionPointers programStates
-        6 -> jumpIfFalse program offset phases amp input output instructionPointers programStates 
-        7 -> lessThan program offset phases amp input output instructionPointers programStates
-        8 -> equals program offset phases amp input output instructionPointers programStates
-        99 -> output
+-- processCode input output offset program
+processCode :: Int -> [Int] -> Int -> [Int] -> [Int]
+processCode input output offset program =
+    let instructionHead = head (drop offset program) 
+        opcode = instructionHead `mod` 10
+        modeParam1 = instructionHead `div` 100 `mod` 10
+        modeParam2 = instructionHead `div` 1000 `mod` 10
+    in
+        case (head (drop offset program)) `mod` 100 of
+            1 -> sum1 program offset input output
+            2 -> multiply program offset input output
+            3 -> input' program offset input output
+            4 -> output' program offset input output
+            5 -> jumpIfTrue program offset input output
+            6 -> jumpIfFalse program offset input output 
+            7 -> lessThan program offset input output
+            8 -> equals program offset input output
+            99 -> output
 
-input' :: [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [[Int]] -> Int               
-input' program ip phases amp input output instructionPointers programStates =
-    let 
-        param1 = (drop ip program) !! 1
-        newProgram = replaceNth param1 inputValue program
-        inputValue = head input
+input' :: [Int] -> Int -> Int -> [Int] -> [Int]              
+input' program ip input output =
+    let (a : b : xs) = drop ip program
+        newProgram = replaceNth b input program   
     in 
-        processCode phases amp (drop 1 input) output (ip + 2) newProgram instructionPointers programStates
+        processCode 0 output (ip + 2) newProgram
 
-output' :: [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [[Int]] -> Int 
-output' program offset phases amp input output instructionPointers programStates =
-    let 
-        (a : b : xs) = drop offset program
-        phase = take 1 phases
-        producedOutput = (program !! b)
-        nextAmp = ((amp + 1) `mod` 5)
 
-        newProgramStates = replaceNth amp program programStates
-        newInstructionPointers = replaceNth amp (offset + 2) instructionPointers
-
-        nextProgram = programStates !! nextAmp
-        instructionPointer = (instructionPointers !! nextAmp)
+output' :: [Int] -> Int -> Int -> [Int] -> [Int]
+output' program ip input output =
+    let (a : b : xs) = drop ip program
     in 
-        processCode 
-            (drop 1 phases) nextAmp (phase ++ [producedOutput]) producedOutput instructionPointer nextProgram newInstructionPointers newProgramStates
+        processCode 0 [program !!b] (ip + 2) program
 
 
-lessThan :: [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [[Int]] -> Int 
-lessThan program ip phases amp input output instructionPointers programStates =
+lessThan :: [Int] -> Int -> Int -> [Int] -> [Int] 
+lessThan program ip input output =
     let param1 = getParam program ip 1
         param2 = getParam program ip 2
         param3 = (drop ip program) !! 3
         newValue = if param1 < param2 then 1 else 0
         newProgram = replaceNth param3 newValue program
     in
-        processCode phases amp input output ip newProgram instructionPointers programStates
+        processCode input output (ip + 4) newProgram
 
 
-equals :: [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [[Int]] -> Int 
-equals program ip phases amp input output instructionPointers programStates =
+equals :: [Int] -> Int -> Int -> [Int] -> [Int]
+equals program ip input output =
     let param1 = getParam program ip 1
         param2 = getParam program ip 2
         param3 = (drop ip program) !! 3
         newValue = if param1 == param2 then 1 else 0
         newProgram = replaceNth param3 newValue program
     in
-        processCode phases amp input output ip newProgram instructionPointers programStates
+        processCode input output (ip + 4) newProgram
 
 
-jumpIfTrue :: [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [[Int]] -> Int 
-jumpIfTrue program ip phases amp input output instructionPointers programStates =
+jumpIfTrue :: [Int] -> Int -> Int -> [Int] -> [Int] 
+jumpIfTrue program ip input output =
     let param1 = getParam program ip 1
         param2 = getParam program ip 2
     in 
         if param1 /= 0 
-            then processCode phases amp input output param2 program instructionPointers programStates
-        else processCode phases amp input output ip program instructionPointers programStates
+            then processCode input output param2 program
+        else processCode input output (ip + 3) program
 
 
-jumpIfFalse :: [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [[Int]] -> Int 
-jumpIfFalse program ip phases amp input output instructionPointers programStates =
+jumpIfFalse :: [Int] -> Int -> Int -> [Int] -> [Int]
+jumpIfFalse program ip input output =
     let param1 = getParam program ip 1
         param2 = getParam program ip 2
     in 
         if param1 == 0 
-            then processCode phases amp input output param2 program instructionPointers programStates
-        else processCode phases amp input output ip program instructionPointers programStates
+            then processCode input output param2 program
+        else processCode input output (ip + 3) program
 
 
 getMode :: Int -> Int -> Int
@@ -138,19 +125,19 @@ getParam program ip offset =
             1 -> program !! paramOffset
 
 
-sum1 :: [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [[Int]] -> Int 
-sum1 program ip phases amp input output instructionPointers programStates =
+sum1 :: [Int] -> Int -> Int -> [Int] -> [Int]
+sum1 program ip input output =
     let param1 = getParam program ip 1
         param2 = getParam program ip 2
         param3 = (drop ip program) !! 3
         calculated = param1 + param2
         newProgram = replaceNth param3 calculated program
     in
-        processCode phases amp input output (ip + 4) newProgram instructionPointers programStates
+        processCode input output (ip + 4) newProgram
 
 
-multiply :: [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> [[Int]] -> Int
-multiply program ip phases amp input output instructionPointers programStates =
+multiply :: [Int] -> Int -> Int -> [Int] -> [Int]
+multiply program ip input output =
     let param1 = getParam program ip 1
         param2 = getParam program ip 2
         param3 = (drop ip program) !! 3
@@ -158,7 +145,7 @@ multiply program ip phases amp input output instructionPointers programStates =
             -- `debug` ("\nhello : " ++ show (ip))
         newProgram = replaceNth param3 calculated program
     in
-        processCode phases amp input output (ip + 4) newProgram instructionPointers programStates
+        processCode input output (ip + 4) newProgram
 
 
 replaceNth :: Int -> a -> [a] -> [a]
@@ -166,5 +153,6 @@ replaceNth _ _ [] = []
 replaceNth n newVal (x:xs)
     | n == 0 = newVal:xs
     | otherwise = x:replaceNth (n-1) newVal xs
+
 
 debug = flip trace
